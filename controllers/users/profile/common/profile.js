9de321b6070af52b8126b720e55812e45104f0d5
radu200 <raduprodan200@gmail.com>
2019-01-20 22:17:22 +0000

    finished reset password func

--- controllers/users/profile/common/profile.js
+++ controllers/users/profile/common/profile.js
@@ -303,7 +303,7 @@ module.exports.postChangePassword =  (req, res, next) => {
 
                 if (result === false) {
                     req.flash('error_msg', {
-                        msg: "Your old password  is wrong.Please try again."
+                        msg: "Parola veche este greșită. Încearcă din nou."
                     })
                     return res.redirect('/password/reset')
                 } else if (result === true) {
@@ -333,10 +333,7 @@ module.exports.postChangePassword =  (req, res, next) => {
                         to: rows[0].email,
                         from: 'JOB APP',
                         subject: 'Parola dvs. a fost schimbată',
-                        html: `<head>
-
-                        <body bgcolor="#e1e5e8" style="margin-top:0 ;margin-bottom:0 ;margin-right:0 ;margin-left:0 ;padding-top:0px;padding-bottom:0px;padding-right:0px;padding-left:0px;background-color:#e1e5e8;">
-                         
+                        html: ` <body bgcolor="#e1e5e8" style="margin-top:0 ;margin-bottom:0 ;margin-right:0 ;margin-left:0 ;padding-top:0px;padding-bottom:0px;padding-right:0px;padding-left:0px;background-color:#e1e5e8;">
                           <center style="width:100%;table-layout:fixed;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;background-color:#e1e5e8;">
                             <div style="max-width:600px;margin-top:0;margin-bottom:0;margin-right:auto;margin-left:auto;">
                               <table align="center" cellpadding="0" style="border-spacing:0;font-family:'Muli',Arial,sans-serif;color:#333333;Margin:0 auto;width:100%;max-width:600px;">
@@ -370,10 +367,7 @@ module.exports.postChangePassword =  (req, res, next) => {
                                           </tr>
                                         </tbody>
                                       </table>
-                                      <!--[if (gte mso 9)|(IE)]>
-                                            </td></tr></table>
-                                            </center>
-                                            <![endif]-->
+                                   
                                     </td>
                                   </tr>
                                   <!-- End of Email Body-->
@@ -403,13 +397,12 @@ module.exports.postChangePassword =  (req, res, next) => {
                                   <tr>
                                     <td style="padding-top:0;padding-bottom:0;padding-right:30px;padding-left:30px;text-align:center;Margin-right:auto;Margin-left:auto;">
                                       <center>
-                                        <p style="font-family:'Muli',Arial,sans-serif;Margin:0;text-align:center;Margin-right:auto;Margin-left:auto;font-size:15px;color:#a1a8ad;line-height:23px;">Problems or questions? Call us at
+                                        <p style="font-family:'Muli',Arial,sans-serif;Margin:0;text-align:center;Margin-right:auto;Margin-left:auto;font-size:15px;color:#a1a8ad;line-height:23px;">Probleme sau intrebari contacteaza-ne la
                                           <nobr><a class="tel" href="tel:2128102899" style="color:#a1a8ad;text-decoration:none;" target="_blank"><span style="white-space: nowrap">212.810.2899</span></a></nobr>
                                         </p>
                         
                                         <p style="font-family:'Muli',Arial,sans-serif;Margin:0;text-align:center;Margin-right:auto;Margin-left:auto;font-size:15px;color:#a1a8ad;line-height:23px;">or email <a href="mailto:hello@vervewine.com" style="color:#a1a8ad;text-decoration:underline;" target="_blank">hello@mail.com</a></p>
                         
-                                        <p style="font-family:'Muli',Arial,sans-serif;Margin:0;text-align:center;Margin-right:auto;Margin-left:auto;padding-top:10px;padding-bottom:0px;font-size:15px;color:#a1a8ad;line-height:23px;">© Verve Wine <span style="white-space: nowrap">24 ​Hubert S​t​</span>, <span style="white-space: nowrap">London</span> <span style="white-space: nowrap">N​Y 1​0013</span></p>
                                       </center>
                                     </td>
                                   </tr>
@@ -425,12 +418,7 @@ module.exports.postChangePassword =  (req, res, next) => {
                               </table>
                             </div>
                           </center>
-                        
-                        
-                        
-                        </body>
-                        `
-                        // text: `Salut,\n\nAceasta este o confirmare că parola pentru contul dvs. tocmai a fost modificată.\n`
+                        </body>`
 
                     };
 
@@ -464,22 +452,7 @@ module.exports.postChangePassword =  (req, res, next) => {
 
 module.exports.getForgotPassword = (req,res,next) => {
     res.render('./profile/common/forgot_password');
-    // db.query('SELECT users.password, users.forgotPasswordtoken , users.forgotPasswordTokenExpires FROM  users WHERE forgotPasswordtoken = ? AND forgotPasswordTokenExpires > NOW()', [req.params.token], function (err, rows, fields) {
-    //     if (err) throw err;
-
-    //     if (!rows.length) {
-    //         req.flash('error_msg', {
-    //             msg: 'Password reset token is invalid or has expired.'
-    //         })
-    //          res.render('./profile/common/forgot_password');
-    //     } else {
-
-    //         res.render('./profile/common/forgot_password', {
-    //             'result': rows[0],
-
-    //         })
-    //     }
-    // })
+   
     
 }
 
@@ -514,7 +487,7 @@ function sendTokenResetPassword(req, res, next) {
                 let token = buffer.toString('hex');
                 // console.log('token',token)
                 let updateToken = {
-                    forgotPasswordtoken:token
+                    forgotPasswordToken:token
                 }
                 db.query('UPDATE users SET ?, forgotPasswordTokenExpires = TIMESTAMPADD(HOUR, 1, NOW())  WHERE email = ? ', [updateToken, email], function (error, result) {
                     if (error) throw error
@@ -677,8 +650,8 @@ function sendTokenResetPassword(req, res, next) {
  module.exports.postForgotPasswordReset = (req, res,next ) => {
     let password = req.body.newPassword;
     let confirm = req.body.confirmNewPassword
-    req.checkBody('password', 'Password must be between 6-100 characters long.').len(6, 100);
-    req.checkBody('confirm', 'Passwords do not match').equals(req.body.password);
+    // req.checkBody('password', 'Password must be between 6-100 characters long.').len(6, 100);
+    // req.checkBody('confirm', 'Passwords do not match').equals(req.body.password);
 
 
     const errors = req.validationErrors();
@@ -689,58 +662,147 @@ function sendTokenResetPassword(req, res, next) {
     }
     /// check for valid token
 
-    db.query('SELECT users.id, users.email,users.first_name,users.password, users.resetPasswordToken ,users.passwordResetExpires,users.type, users.user_status FROM  users WHERE resetPasswordToken = ? AND passwordResetExpires > NOW()', [req.params.token], function (err, rows, fields) {
+    db.query('SELECT users.id, users.email,users.first_name,users.password, users.forgotPasswordtoken,users.forgotPasswordTokenExpires,users.type  FROM  users WHERE forgotPasswordToken = ? AND forgotPasswordTokenExpires > NOW()', [req.params.token], function (err, rows, fields) {
         if (err) {
             console.log('[mysql error]', err)
         }
+
         let email = rows[0].email
-        let user = rows[0]
+          let user = rows[0]
+
         console.log('user', user)
+
         if (!rows.length) {
             req.flash('error_msg', {
-                msg: 'Password reset token is invalid or has expired.'
+                msg: 'Resetarea parolei nu este validă sau a expirat.'
             })
-            res.redirect('/forgot')
+            res.redirect('/forgot/password')
 
         }
-        //    
-        ///hash and update password
+           
+        //hash and update password
         bcrypt.hash(password, saltRounds, function (err, hash) {
-            db.query('UPDATE  users SET password = ? WHERE resetPasswordToken = ? AND passwordResetExpires > NOW()', [hash, req.params.token], function (error, result) {
+            db.query('UPDATE  users SET password = ? WHERE forgotPasswordToken = ? AND forgotPasswordTokenExpires > NOW()', [hash, req.params.token], function (error, result) {
                 if (error) throw error
                 console.log('updated')
 
-
-
-
-
                 //send email that password was updated
                 const transwerporter = nodemailer.createTransport({
-                    service: 'MAILGUN',
+                    service: 'GMAIL',
                     auth: {
-                        user: process.env.MAILGUN_USER,
-                       pass: process.env.MAILGUN_PASSWORD
+                        user: process.env.MAIL_USER,
+                       pass: process.env.MAIL_PASSWORD
                     }
                 });
 
                 const mailOptions = {
                     to: email,
-                    from: 'Company ecomerce',
-                    subject: 'Your password has been changed',
-                    text: `Hello,\n\nThis is a confirmation that the password for your account  has just been changed.\n`
+                    from: 'JOB APP',
+                    subject: 'Parola dvs. a fost resetata cu success',
+                     html: `<body bgcolor="#e1e5e8" style="margin-top:0 ;margin-bottom:0 ;margin-right:0 ;margin-left:0 ;padding-top:0px;padding-bottom:0px;padding-right:0px;padding-left:0px;background-color:#e1e5e8;">                         
+                    <center style="width:100%;table-layout:fixed;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;background-color:#e1e5e8;">
+                      <div style="max-width:600px;margin-top:0;margin-bottom:0;margin-right:auto;margin-left:auto;">
+                        <table align="center" cellpadding="0" style="border-spacing:0;font-family:'Muli',Arial,sans-serif;color:#333333;Margin:0 auto;width:100%;max-width:600px;">
+                          <tbody>
+                            <tr>
+                              <td align="center"><h1 style="color:white;background: red; padding: 20px 0; margin: 50px; width:100px">JOB APP</h1></td>
+                            </tr>
+                            <!-- Start of Email Body-->
+                            <tr>
+                              <td class="one-column" style="padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;background-color:#ffffff;">
+                             
+                                <table style="border-spacing:0;" width="100%">
+                                  <tbody>
+                                  
+                                    <tr>
+                                      <td class="inner contents center" style="padding-top:15px;padding-bottom:15px;padding-right:30px;padding-left:30px;text-align:left;">
+                                        <center>
+                                          <p class="h1 center" style="Margin:0;text-align:center;font-family:'flama-condensed','Arial Narrow',Arial;font-weight:100;font-size:30px;Margin-bottom:26px;">Parola dvs. a fost schimbată</p>
+                                          <!--[if (gte mso 9)|(IE)]><![endif]-->
+                  
+                                          <p class="description center" style="font-family:'Muli','Arial Narrow',Arial;Margin:0;text-align:center;max-width:320px;color:#a1a8ad;line-height:24px;font-size:15px;Margin-bottom:10px;margin-left: auto; margin-right: auto;"><span style="color: rgb(161, 168, 173); font-family: Muli, &quot;Arial Narrow&quot;, Arial; font-size: 15px; text-align: center; background-color: rgb(255, 255, 255);">Aceasta este o confirmare că parola pentru contul dvs. tocmai a fost modificată</span></p>
+                                          <p style="font-family:'Muli',Arial,sans-serif;Margin:0;text-align:center;Margin-right:auto;Margin-left:auto;font-size:15px;color:#a1a8ad;line-height:23px;">Daca nu a fost dvs. va rog sa ne contactati la:
+                                              <nobr><a class="tel" href="tel:2128102899" style="color:#a1a8ad;text-decoration:none;" target="_blank"><span style="white-space: nowrap">212.810.2899</span></a></nobr>
+                                            </p>
+                            
+                                            <p style="font-family:'Muli',Arial,sans-serif;Margin:0;text-align:center;Margin-right:auto;Margin-left:auto;font-size:15px;color:#a1a8ad;line-height:23px;">or email <a href="mailto:hello@vervewine.com" style="color:#a1a8ad;text-decoration:underline;" target="_blank">hello@mail.com</a></p>
+                                        
+                                          <!-- <button style="border: none; color:white; background-color:#2196F3 ;padding:14px 60px;cursor: pointer; display: inline-block;">Reset password</button> -->
+                                        </center>
+                                      </td>
+                                    </tr>
+                                  </tbody>
+                                </table>
+                             
+                              </td>
+                            </tr>
+                            <!-- End of Email Body-->
+                            <!-- whitespace -->
+                            <tr>
+                              <td height="40">
+                                <p style="line-height: 40px; padding: 0 0 0 0; margin: 0 0 0 0;">&nbsp;</p>
+                  
+                                <p>&nbsp;</p>
+                              </td>
+                            </tr>
+                            <!-- Social Media -->
+                            <tr>
+                              <td align="center" style="padding-bottom:0;padding-right:0;padding-left:0;padding-top:0px;" valign="middle"><span class="sg-image" data-imagelibrary="%7B%22width%22%3A%228%22%2C%22height%22%3A18%2C%22alt_text%22%3A%22Facebook%22%2C%22alignment%22%3A%22%22%2C%22border%22%3A0%2C%22src%22%3A%22https%3A//marketing-image-production.s3.amazonaws.com/uploads/0a1d076f825eb13bd17a878618a1f749835853a3a3cce49111ac7f18255f10173ecf06d2b5bd711d6207fbade2a3779328e63e26a3bfea5fe07bf7355823567d.png%22%2C%22link%22%3A%22%23%22%2C%22classes%22%3A%7B%22sg-image%22%3A1%7D%7D"><a href="https://www.facebook.com/vervewine/" target="_blank"><img alt="Facebook" height="18" src="https://marketing-image-production.s3.amazonaws.com/uploads/0a1d076f825eb13bd17a878618a1f749835853a3a3cce49111ac7f18255f10173ecf06d2b5bd711d6207fbade2a3779328e63e26a3bfea5fe07bf7355823567d.png" style="border-width: 0px; margin-right: 21px; margin-left: 21px; width: 8px; height: 18px;" width="8"></a></span>
+                                <!--[if gte mso 9]>&nbsp;&nbsp;&nbsp;<![endif]--><span class="sg-image" data-imagelibrary="%7B%22width%22%3A%2223%22%2C%22height%22%3A18%2C%22alt_text%22%3A%22Twitter%22%2C%22alignment%22%3A%22%22%2C%22border%22%3A0%2C%22src%22%3A%22https%3A//marketing-image-production.s3.amazonaws.com/uploads/6234335b200b187dda8644356bbf58d946eefadae92852cca49fea227cf169f44902dbf1698326466ef192bf122aa943d61bc5b092d06e6a940add1368d7fb71.png%22%2C%22link%22%3A%22%23%22%2C%22classes%22%3A%7B%22sg-image%22%3A1%7D%7D"><a href="https://twitter.com/vervewine" target="_blank"><img alt="Twitter" height="18" src="https://marketing-image-production.s3.amazonaws.com/uploads/6234335b200b187dda8644356bbf58d946eefadae92852cca49fea227cf169f44902dbf1698326466ef192bf122aa943d61bc5b092d06e6a940add1368d7fb71.png" style="border-width: 0px; margin-right: 16px; margin-left: 16px; width: 23px; height: 18px;" width="23"></a></span>
+                                <!--[if gte mso 9]>&nbsp;&nbsp;&nbsp;&nbsp;<![endif]--><span class="sg-image" data-imagelibrary="%7B%22width%22%3A%2218%22%2C%22height%22%3A18%2C%22alt_text%22%3A%22Instagram%22%2C%22alignment%22%3A%22%22%2C%22border%22%3A0%2C%22src%22%3A%22https%3A//marketing-image-production.s3.amazonaws.com/uploads/650ae3aa9987d91a188878413209c1d8d9b15d7d78854f0c65af44cab64e6c847fd576f673ebef2b04e5a321dc4fed51160661f72724f1b8df8d20baff80c46a.png%22%2C%22link%22%3A%22%23%22%2C%22classes%22%3A%7B%22sg-image%22%3A1%7D%7D"><a href="https://www.instagram.com/vervewine/" target="_blank"><img alt="Instagram" height="18" src="https://marketing-image-production.s3.amazonaws.com/uploads/650ae3aa9987d91a188878413209c1d8d9b15d7d78854f0c65af44cab64e6c847fd576f673ebef2b04e5a321dc4fed51160661f72724f1b8df8d20baff80c46a.png" style="border-width: 0px; margin-right: 16px; margin-left: 16px; width: 18px; height: 18px;" width="18"></a></span></td>
+                            </tr>
+                            <!-- whitespace -->
+                            <tr>
+                              <td height="25">
+                                <p style="line-height: 25px; padding: 0 0 0 0; margin: 0 0 0 0;">&nbsp;</p>
+                  
+                                <p>&nbsp;</p>
+                              </td>
+                            </tr>
+                            <!-- Footer -->
+                            <tr>
+                              <td style="padding-top:0;padding-bottom:0;padding-right:30px;padding-left:30px;text-align:center;Margin-right:auto;Margin-left:auto;">
+                                <center>
+                                  <p style="font-family:'Muli',Arial,sans-serif;Margin:0;text-align:center;Margin-right:auto;Margin-left:auto;font-size:15px;color:#a1a8ad;line-height:23px;">Probleme sau intrebari contacteaza-ne la
+                                    <nobr><a class="tel" href="tel:2128102899" style="color:#a1a8ad;text-decoration:none;" target="_blank"><span style="white-space: nowrap">212.810.2899</span></a></nobr>
+                                  </p>
+                  
+                                  <p style="font-family:'Muli',Arial,sans-serif;Margin:0;text-align:center;Margin-right:auto;Margin-left:auto;font-size:15px;color:#a1a8ad;line-height:23px;">or email <a href="mailto:hello@vervewine.com" style="color:#a1a8ad;text-decoration:underline;" target="_blank">hello@mail.com</a></p>
+                  
+                                </center>
+                              </td>
+                            </tr>
+                            <!-- whitespace -->
+                            <tr>
+                              <td height="40">
+                                <p style="line-height: 40px; padding: 0 0 0 0; margin: 0 0 0 0;">&nbsp;</p>
+                  
+                                <p>&nbsp;</p>
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </div>
+                    </center>
+                  </body>`
+
+                    
+                    
+                  
+
 
                 };
 
                 transwerporter.sendMail(mailOptions, (err) => {
                     if (err) {
                         req.flash('error_msg', errors)
-                        return res.redirect('/forgot');
+                        return res.redirect('/forgot/password');
                     }
                 });
 
                 req.login(user, function (err) {
                     req.flash('success_msg', {
-                        msg: 'Success! Your password has been changed.'
+                        msg: 'Parola dvs. a fost schimbată.'
                     });
                     res.redirect('/profile')
                 });
