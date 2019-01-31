b912ed9459cf7522f118551b1132fe525d53642c
radu200 <raduprodan200@gmail.com>
2019-01-31 00:06:39 +0000

    check form values in backend

--- server/controllers/users/profile/job_seeker/profile.js
+++ server/controllers/users/profile/job_seeker/profile.js
@@ -12,7 +12,27 @@ module.exports.getJobSeekerProfileInfoEdit = (req,res,next) => {
 
 
 module.exports.postJobSeekerProfileInfoEdit = (req,res,next) => {
-   console.log('lalal')
+   const firstName = req.body.firstName;
+   const lastName = req.body.lastName;
+   const availabilityJobseeker = req.body.availabilityJobseeker;
+   const employementType = req.body.employementType;
+   const jobseekerLocation = req.body.jobseekerLocation;
+   const jobseekerEmail = req.body.jobseekerEmail;
+   const phoneNumber = req.body.phoneNumber;
+   const jobseekerDescription = req.body.jobseekerDescription;
+   const language = req.body.language;
+   const education = req.body.education;
+
+   console.log(firstName)
+   console.log(lastName);
+   console.log(availabilityJobseeker);
+   console.log(employementType);
+   console.log(jobseekerLocation)
+   console.log(jobseekerEmail);
+   console.log(phoneNumber);
+   console.log(jobseekerDescription);
+   console.log(language);
+   console.log(education)
 }
 
 
@@ -22,5 +42,19 @@ module.exports.getJobSeekerProfileEditExperience = (req,res,next) => {
 }
 
 module.exports.postJobSeekerProfileEditExperience = (req,res,next) => {
-   console.log('working experi')
+  
+   const experience = req.body.categoryExperience;
+   const position = req.body.position;
+   const companyName = req.body.companyName
+   const startDate = req.body.startDate;
+   const endDate = req.body.endDate;
+   const mainResposibilities =  req.body.mainResposibilities;
+
+   console.log(experience);
+   console.log(position)
+   console.log(companyName)
+   console.log('start',startDate)
+   console.log('end',endDate)
+   console.log(mainResposibilities)
+    
 }
