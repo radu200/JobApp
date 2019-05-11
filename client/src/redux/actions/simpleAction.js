d5bcf2cac64d8a5549c1ced0920597fe4ee4bc16
radu200 <raduprodan200@gmail.com>
2019-05-12 00:40:04 +0100

    new action

--- client/src/redux/actions/simpleAction.js
+++ client/src/redux/actions/simpleAction.js
@@ -4,3 +4,10 @@ export const simpleAction = () => dispatch => {
    payload: 'result_of_simple_action'
   })
  }
+
+ export const simpleAction2 = () => dispatch => {
+  dispatch({
+   type: 'SIMPLE_ACTION',
+   payload: 'result_of_simple_action'
+  })
+ }
